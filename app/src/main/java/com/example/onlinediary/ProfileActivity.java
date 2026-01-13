fdf5be5052e7b8ee2cb5d9e4f6b806a192c60a5c
Николай Филиппов <filippovnikolay195@gmail.com>
2026-01-13 18:30:56 +0200

    fixed build

--- app/src/main/java/com/example/onlinediary/ProfileActivity.java
+++ app/src/main/java/com/example/onlinediary/ProfileActivity.java
@@ -12,6 +12,7 @@ import android.util.Base64;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -281,7 +282,7 @@ public class ProfileActivity extends AppCompatActivity {
             headerMenu = new PopupWindow(
                     menuView,
                     dpToPx(240),
-                    PopupWindow.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
                     true
             );
             headerMenu.setOutsideTouchable(true);
